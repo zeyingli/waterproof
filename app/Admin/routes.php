9dea078d886c0ec598e34ee90b6d7c52fdf98e2f
Zeying Li <admin@zeyingli.com>
2018-11-13 19:06:37 -0500

    rewrote router method

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,20 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('/summary', 'HomeController@summary');
 
+    // Operations Class
+    $router->resources([
+    	'/operations/kiosk' 		=> KioskController::class,
+    	'/operations/umbrella' 		=> UmbrellaController::class, 
+    	'/operations/record' 		=> RecordController::class,
+    	'/operations/transaction' 	=> TransactionController::class,
+    	'/operations/vendor' 		=> VendorController::class, 
+    ]);
+
+    // API Route
+    $router->get('/api/get-kiosks', 'KioskController@getKiosks');
+    $router->get('/api/get-availableUmbrella', 'UmbrellaController@getAvailableUmbrella');
+
+    // Custom Route
+    $router->post('/operations/kiosk/status', 'KioskController@status');
+
 });
