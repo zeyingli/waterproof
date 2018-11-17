16b96caec122e4ae342ea427531921a4dabcbcd4
Zeying Li <admin@zeyingli.com>
2018-11-16 20:45:39 -0500

    record creation status default set to pending

--- database/migrations/2018_11_14_005630_add_record_table.php
+++ database/migrations/2018_11_14_005630_add_record_table.php
@@ -19,11 +19,12 @@ class AddRecordTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('kiosk_id')->unsigned()->index();
             $table->foreign('kiosk_id')->references('id')->on('kiosk');
+            $table->string('return_kiosk')->after('kiosk_id')->nullable();
             $table->integer('umbrella_id')->unsigned()->index();
             $table->foreign('umbrella_id')->references('id')->on('umbrella');
             $table->datetime('start_time')->nullable();
             $table->datetime('end_time')->nullable();
-            $table->tinyInteger('status')->unsigned();
+            $table->tinyInteger('status')->unsigned()->default('0');
             $table->timestamps();
         });
     }
