3126b0c26b0e8a8ebd6b0826c0dcf7e1c1c420b5
Zeying Li <admin@zeyingli.com>
2018-11-18 00:53:12 -0500

    update favicon at landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -27,7 +27,7 @@
     <meta property="og:image" content="{{ config('app.cdn', 'https://cdn.zeyingli.com/waterproof/') }}" />
 
     {{-- Favicon --}}
-    <link rel="shortcut icon" href="{{ config('app.cdn') }}images/favicon/favicon.ico?version={{ config('app.version') }}">
+    <link rel="shortcut icon" href="{{ config('app.cdn') }}images/favicon/apple-touch-icon.png?version={{ config('app.version') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('app.cdn') }}images/favicon/apple-touch-icon.png?version={{ config('app.version') }}">
 	<link rel="icon" type="image/png" sizes="32x32" href="{{ config('app.cdn') }}images/favicon/favicon-32x32.png?version={{ config('app.version') }}">
 	<link rel="icon" type="image/png" sizes="16x16" href="{{ config('app.cdn') }}images/favicon/favicon-16x16.png?version={{ config('app.version') }}">
