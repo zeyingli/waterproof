5fb3ae7907a03ec9cd1faf791494de5b05593579
Zeying Li <admin@zeyingli.com>
2018-11-19 01:19:32 -0500

    added settings config file

---
+++ config/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	/*
+     * Google Maps API V3 Status
+     */
+    'googleMapsAPIStatus' => env('GOOGLEMAPS_API_STATUS', false),
+    /*
+     * Google Maps API Key
+     */
+    'googleMapsAPIKey'    => env('GOOGLEMAPS_API_KEY', 'YOURGOOGLEMAPSkeyHERE'),
+
+];
