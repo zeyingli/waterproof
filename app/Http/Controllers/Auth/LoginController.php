4df3751f39c220270dd6fdade39d010e0bd5018b
Zeying Li <admin@zeyingli.com>
2018-11-19 21:16:23 -0500

    allowed using email or username or phone to log=in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -25,7 +28,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
+    protected $redirectAfterLogout = '/login';
 
     /**
      * Create a new controller instance.
@@ -35,5 +39,35 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+       * Get the needed authorization credentials from the request.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @return array
+       */
+    protected function credentials(Request $request)
+    {
+        if(is_numeric($request->get('email')))
+        {
+            return ['phone'=>$request->get('email'),'password'=>$request->get('password')];
+        } elseif (filter_var($request->get('email'), FILTER_VALIDATE_EMAIL)) 
+        {
+            return ['email' => $request->get('email'), 'password'=>$request->get('password')];
+        }
+        return ['username' => $request->get('email'), 'password'=>$request->get('password')];
+    }
+
+    /**
+     * Logout, Clear Session, and Return.
+     *
+     * @return void
+     */
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
     }
 }
