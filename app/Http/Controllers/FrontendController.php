de34fddcbfb8b7fbf6adcf5f520514dc053c9ae8
Zeying Li <admin@zeyingli.com>
2018-11-26 14:40:50 -0500

    allowed rendering available kiosk location with marker

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administration\Kiosk;
 use Cornford\Googlmapper\Facades\MapperFacade as Mapper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FrontendController extends Controller
 {
@@ -25,6 +27,19 @@ class FrontendController extends Controller
     public function dashboard()
     {
         Mapper::map(53.381128999999990000, -1.470085000000040000);
+        $kiosks = Kiosk::where('status', 1)->get();
+
+        Mapper::map(40.7964652, -77.86278949);
+
+        foreach ($kiosks as $kiosk) 
+        {
+            $umbrella = DB::table('umbrella')->join('kiosk', 'umbrella.kiosk_id', '=', 'kiosk.id')->where([
+                ['umbrella.kiosk_id', '=', $kiosk->id],
+                ['umbrella.status', '=', 0],
+            ])->count();
+            Mapper::informationWindow($kiosk->lat, $kiosk->lng, 'Available Umbrella: '. $umbrella, ['open' => false, 'maxWidth' => 200, 'markers' => ['title' => 'Available Kiosk']]);
+        }
+
         return view('frontend.dashboard');
     }
 
