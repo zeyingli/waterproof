98b6b5fa235c82ca73124b90c95897666226c7d4
Zeying Li <admin@zeyingli.com>
2018-11-28 21:40:04 -0500

    added few entries for pickup and dropoff

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,22 @@ Route::get('/logout', ['uses' => 'Auth\LoginController@logout'])->name('User Log
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
     
+    // Menu
     Route::get('/dashboard', 'FrontendController@dashboard')->name('User Dashboard');
-    Route::get('/pickup/{id}', 'FrontendController@pickup')->name('Pickup Umbrella');
-    
     Route::get('/account', 'FrontendController@account')->name('My Account');
+    Route::get('/help', 'FrontendController@help')->name('Live Help');
+
+    // Dashboard Multi-tabs
+    Route::get('/pickup/{id}', 'FrontendController@pickup')->name('Pickup Umbrella');
+    Route::post('/pickup/{id}', 'FrontendController@pickupUmbrella')->name('Picking up Umbrella');
+    Route::get('/dropoff/{id}', 'FrontendController@dropoff')->name('Dropoff Umbrella');
+    Route::post('/dropoff/{id}', 'FrontendController@dropoffUmbrella')->name('Droping off Umbrella');
+    Route::get('/ontrip', 'FrontendController@ontrip')->name('On Trip');
+    
+    // User Account
     Route::get('/account/recharge', 'FrontendController@recharge')->name('Recharge Account');
     Route::post('/account/recharge', 'FrontendController@addbalance')->name('Add Balance');
     Route::get('/account/history', 'FrontendController@history')->name('Payment History');
+    Route::post('/account/pay/{id}', 'FrontendController@payOverduedOrder')->name('Pay Overdued Order');
 });
 
